e21b1bb60d9c9569464c0068547659479c2bfebb
HafizzN <Hafizulhanifr@gmail.com>
2024-05-09 07:30:58 +0700

    Update newFile.js

--- newFile.js
+++ newFile.js
@@ -5,7 +5,7 @@ window.addEventListener('load', () => {
                 // text: "You won't be able to revert this!",
                 icon: 'warning',
                 showCancelButton: true,
-                confirmButtonColor: '#3085',
+                confirmButtonColor: '#d69a39',
                 cancelButtonColor: '#d364',
                 confirmButtonText: 'Yes',
                 cancelButtonText: 'No',

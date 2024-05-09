79905bb37ec0165ad699eff69fe6318bdd8ff10a
HafizzN <Hafizulhanifr@gmail.com>
2024-05-09 07:22:58 +0700

    Add files via upload

---
+++ newFile.js
@@ -0,0 +1,20 @@
+// trigger to play music in the background with sweetalert
+window.addEventListener('load', () => {
+        Swal.fire({
+                title: 'Do you want to play music in the background?',
+                // text: "You won't be able to revert this!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085',
+                cancelButtonColor: '#d364',
+                confirmButtonText: 'Yes',
+                cancelButtonText: 'No',
+        }).then((result) => {
+                if (result.isConfirmed) {
+                        document.querySelector('.song').play();
+                        resolveFetch().then(animationTimeline());
+                } else {
+                        resolveFetch().then(animationTimeline());
+                }
+        });
+});
